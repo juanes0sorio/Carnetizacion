a45807b03d50f244c055f5ec42a1a669cd0ab21e
Juan Osorio <juanestebanosorio24@gmail.com>
2025-08-05 20:16:19 -0400

    Retorna archivos en un .zip

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.responses import FileResponse
 from pydantic import BaseModel
 from generadorcarnet import GeneradorCarnet
+import zipfile
+import tempfile
 app = FastAPI()
 
 class Carnetizacion(BaseModel):
@@ -13,8 +16,15 @@ class Carnetizacion(BaseModel):
 def generar_carnet(datos: Carnetizacion):
     try:
         generador = GeneradorCarnet(datos.dict())
+        cedula = datos.Cedula
         generador.generar_carnetizacion()
-        return {"mensaje": "Carnet generado correctamente", "cedula": datos.Cedula}
+
+        temp_zip = tempfile.NamedTemporaryFile(delete=False, suffix=".zip")
+        with zipfile.ZipFile(temp_zip.name, 'w') as zipf:
+            zipf.write(f"{cedula}_frontal.png", arcname="frontal.png")
+            zipf.write(f"{cedula}_trasera.png", arcname="trasera.png")
+
+        return FileResponse(temp_zip.name, media_type='application/zip', filename=f"carnet_{cedula}.zip")
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
